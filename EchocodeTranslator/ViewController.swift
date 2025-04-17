f6b68a82b15bd06764e0133ae545d22a0d4c6517
larioncon <hilltom@protonmail.com>
2025-04-17 12:15:28 +0300

    Initial Commit

---
+++ EchocodeTranslator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EchocodeTranslator
+//
+//  Created by Chmil Oleksandr on 17/04/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
